c331febaa49be51448ca38a79027d426b3fe9b57
Chris <scharl.chrissi2006@gmail.com>
2023-10-28 17:35:37 +0200

    Create general.js

---
+++ admin/scripts/general.js
@@ -0,0 +1,22 @@
+const navDropdown = document.getElementsByClassName("nav-dropdown")[0];
+const navSubMenu = document.getElementsByClassName("nav-sub-menu")[0];
+const bigNavDropDown = document.getElementsByClassName("nav-big-dropdown")[0];
+const bigNavSubMenu = document.getElementsByClassName("nav-big-sub-menu")[0];
+
+let navToggle = false;
+
+navDropdown.onclick = (event) => {
+    navToggle = !navToggle;
+
+    if (navToggle) navSubMenu.classList.add("show")
+    else navSubMenu.classList.remove("show");
+}
+
+let bigNavToggle = false;
+
+bigNavDropDown.onclick = (event) => {
+    bigNavToggle = !bigNavToggle;
+
+    if (bigNavToggle) bigNavSubMenu.classList.add("show")
+    else bigNavSubMenu.classList.remove("show");
+}
