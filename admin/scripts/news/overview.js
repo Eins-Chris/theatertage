90444170289dd2356f97aac218a99581065e3f97
Angelo <anweisen@web.de>
2024-01-26 16:00:48 +0100

    Make the overview clickable

--- admin/scripts/news/overview.js
+++ admin/scripts/news/overview.js
@@ -3,22 +3,25 @@ async function createNewsOverview() {
 
   // skip first row (titles/names)
   for (let i = 1; i < data.length; i++) {
-    createNewsOverviewFor(data[i]);
+    createNewsOverviewFor(i, data[i]);
   }
 }
 
 const newsContainer = document.getElementById("news");
 
-function createNewsOverviewFor(post) {
+function createNewsOverviewFor(index, post) {
   // create element & load preset
   const element = $("<div>");
   const jqElement = $(element);
   jqElement.load("/admin/preset/news_overview.html", () => {
-    jqElement.find(".title").text(post[NewsRow.Headline])
-    jqElement.find(".date").text(dateFormat.format(post[NewsRow.Date]))
-    jqElement.find(".summary").text(post[NewsRow.Summary])
+    const entry = jqElement.find(".entry");
 
-    $(newsContainer).append(jqElement.find(".entry"));
+    entry.find(".title").text(post[NewsRow.Headline])
+    entry.find(".date").text(dateFormat.format(post[NewsRow.Date]))
+    entry.find(".summary").text(post[NewsRow.Summary])
+    entry.attr("href", `./post?id=${index}`)
+    
+    $(newsContainer).append(entry);
   })
 }
 
